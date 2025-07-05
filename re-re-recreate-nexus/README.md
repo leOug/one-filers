25a1f97076e45a538cc6a34a73cb109be11316c0
thedude <xrisostomos.kinalidis@gmail.com>
2025-07-06 02:36:01 +0300

    new: Initial concept for provisioning a Nexus instance

---
+++ re-re-recreate-nexus/README.md
@@ -0,0 +1,28 @@
+A Very Important Bucket
+---
+
+#### Prerequisites
+
+This was executed against a **Sonatype Nexus Repository Community Edition (CE) v3.81.1-01**.
+ Your experience may vary.
+
+**This procedure assumes** that the Nexus repository VM exists and also that **the api is accessible
+through https**. Personally **I have set up a Nexus behind a Traefik** that acts as a reverse proxy
+so that I can have Let's Encrypt TLS and finally avoid having to specify a port other than the default 443. 
+
+The Traefik configuration is beyond the scope of this project. You can always set up nexus as is and
+access it and its registries through the various ports without reverse proxying.
+
+#### Getting started
+
+**This module can be run directly after Sonatype Nexus is initialized**. The **initial admin password**, 
+can be used directly **without the need of changing it**. This helps if you want to parameterize Nexus
+with minimal interaction, and then change the admin password after everything is donw.
+
+The **initial admin password** is usually located under:
+
+```/opt/sonatype-work/nexus3/admin.password``` 
+
+**until the first login**, during which you are prompted to change it.
+
+
---
+++ re-re-recreate-nexus/nexus.tf
@@ -0,0 +1,157 @@
+terraform{
+    required_providers {
+    nexus = {
+      source = "datadrivers/nexus"
+      version = "2.6.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+
+provider "nexus" {
+  # I am using Traefik in order to use https with a valid certificate, so I do not need insecure
+  # insecure = true
+  password = var.nexus_admin_password
+  url      = var.nexus_url
+  username = "admin"
+}
+
+variable "nexus_url" {
+  description = "The url that nexus is accessible"
+  type = string
+  default = "https://nexus.my.doma.in"
+}
+
+variable "nexus_admin_password" {
+  description = "The admin password for Nexus"
+  type = string
+  default = "83f43c14-4c82-46f0-a5e4-72bf5ee1e6f2"
+}
+
+resource "nexus_blobstore_file" "docker-internal" {
+  name = "docker-internal"
+  path = "/nexus-data/docker-internal"
+
+  soft_quota {
+    limit = 1024000000
+    type  = "spaceRemainingQuota"
+  }
+}
+
+resource "nexus_blobstore_file" "dockerhub" {
+  name = "dockerhub"
+  path = "/nexus-data/dockerhub"
+
+  soft_quota {
+    limit = 1024000000
+    type  = "spaceRemainingQuota"
+  }
+}
+
+resource "nexus_blobstore_file" "docker-group" {
+  name = "docker-group"
+  path = "/nexus-data/docker-group"
+
+  soft_quota {
+    limit = 1024000000
+    type  = "spaceRemainingQuota"
+  }
+}
+
+data "nexus_blobstore_file" "docker-internal" {
+  name = nexus_blobstore_file.docker-internal.name
+}
+
+data "nexus_blobstore_file" "dockerhub" {
+  name = nexus_blobstore_file.dockerhub.name
+}
+
+data "nexus_blobstore_file" "docker-group" {
+  name = nexus_blobstore_file.docker-group.name
+}
+
+resource "nexus_repository_docker_hosted" "internal" {
+  name = "internal"
+
+  docker {
+    force_basic_auth = true
+    http_port        = 8083
+    https_port       = 8483
+    v1_enabled       = false
+  }
+
+  storage {
+    blob_store_name                = data.nexus_blobstore_file.docker-internal.name
+    strict_content_type_validation = true
+    write_policy                   = "ALLOW"
+  }
+}
+
+resource "nexus_repository_docker_proxy" "dockerhub" {
+  name = "dockerhub"
+
+  docker {
+    force_basic_auth = false
+    http_port        = 8082
+    v1_enabled       = false
+  }
+
+  docker_proxy {
+    index_type = "HUB"
+  }
+
+  storage {
+    blob_store_name                = data.nexus_blobstore_file.dockerhub.name
+    strict_content_type_validation = true
+  }
+
+  proxy {
+    remote_url       = "https://registry-1.docker.io"
+    content_max_age  = 1440
+    metadata_max_age = 1440
+  }
+
+  negative_cache {
+    enabled = true
+    ttl     = 1440
+  }
+
+  http_client {
+    blocked    = false
+    auto_block = true
+  }
+}
+
+resource "nexus_repository_docker_group" "docker-group" {
+  name   = "docker-group"
+  online = true
+
+  docker {
+    force_basic_auth = false
+    http_port        = 8084
+    https_port       = 8484
+    v1_enabled       = false
+    subdomain        = "docker-group"
+  }
+
+  group {
+    member_names = [
+      nexus_repository_docker_hosted.internal.name,
+      nexus_repository_docker_proxy.dockerhub.name
+    ]
+    # Removed because this is NOT a pro nexus installation
+    # writable_member = nexus_repository_docker_hosted.internal.name
+  }
+
+  storage {
+    blob_store_name                = data.nexus_blobstore_file.docker-group.name
+    strict_content_type_validation = true
+  }
+}
+
+output "docker-hosted-internal-url" {
+  value = "${var.nexus_url}/repository/${nexus_repository_docker_hosted.internal.id}"
+}
